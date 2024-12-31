e4fa1445f11c3d890026948d02b7baeb56be0299
LorenWang <wangliang_workjob@163.com>
2024-12-31 14:32:17 +0800

    顶部导航栏图标调整

--- packages/agora-classroom-sdk/src/infra/capabilities/containers/hand-up/index.tsx
+++ packages/agora-classroom-sdk/src/infra/capabilities/containers/hand-up/index.tsx
@@ -3,8 +3,9 @@ import { EduClassroomConfig, EduRoleTypeEnum } from 'agora-edu-core';
 import { observer } from 'mobx-react';
 import { visibilityControl, raiseHandEnabled } from 'agora-common-libs';
 import './index.css';
-import { StudentsWaveArmList, WaveArmManager } from './manager';
-import { WaveArmSender as WaveArmSenderContainer } from './sender';
+import { StudentsWaveArmList, WaveArmManager, WaveArmManagerNav } from './manager';
+import { WaveArmSender as WaveArmSenderContainer,WaveArmSenderNav as WaveArmSenderContainerNav } from './sender';
+import { Card, Popover, SvgIcon, SvgIconEnum, SvgImg } from '@classroom/ui-kit/components';
 
 export const WaveArmManagerContainer = observer(() => {
   const { handUpUIStore } = useStore();
@@ -32,6 +33,39 @@ export const HandsUpContainer = visibilityControl(
     }
     if (userRole === EduRoleTypeEnum.student) {
       return <WaveArmSenderContainer />;
+    }
+    return null;
+  }),
+  raiseHandEnabled,
+);
+
+export const HandsUpContainerNav = visibilityControl(
+  observer(() => {
+    const userRole = EduClassroomConfig.shared.sessionInfo.role;
+    if (userRole === EduRoleTypeEnum.teacher) {
+      return <div onClick={() => {
+        // 获取目标 div 元素
+        const targetDiv = document.getElementsByClassName('card-hands-up');
+        if (targetDiv && targetDiv.length > 0) {
+          (targetDiv[0] as HTMLElement).click(); // 触发目标 div 的点击事件
+        }
+      }}>
+        <Card
+          className="hands-up-sender-nav"
+          width={20}
+          height={20}
+          borderRadius={20}>
+          <div>
+            <SvgImg
+              type={SvgIconEnum.HANDS_UP}
+              size={20}
+            />
+          </div>
+        </Card>
+      </div>;
+    }
+    if (userRole === EduRoleTypeEnum.student) {
+      return <WaveArmSenderContainerNav />;
     }
     return null;
   }),
