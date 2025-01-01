3bc6a872093cce1a9282b97ab6420107a2f2d1f8
wangliang <wangliang_workjob@163.com>
2025-01-01 12:30:49 +0800

    修改mac地址获取方式

--- packages/agora-demo-app/electron/ipc-listeners/alex-utils.js
+++ packages/agora-demo-app/electron/ipc-listeners/alex-utils.js
@@ -6,27 +6,27 @@ function checkMac() {
   let macAddress;
   for (let iface of Object.values(interfaces)) {
     for (let alias of iface) {
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal && alias.mac && alias.mac !== '00:00:00:00:00:00') {
         macAddress = alias.mac;
         break;
       }
     }
-    if (macAddress) break;
+    if (macAddress)  break;
   }
-  if(!macAddress){
-    macAddress = electron.ipcRenderer.invoke('get-mac-address');
-  }
-  if(!macAddress){
-    for (const iface in interfaces) {
-      for (const ifaceDetails of interfaces[iface]) {
-        // 检查接口类型和 MAC 地址
-        if (ifaceDetails.mac && ifaceDetails.mac !== '00:00:00:00:00:00') {
-          macAddress = ifaceDetails.mac;  // 返回第一个有效的 MAC 地址
-          return macAddress;
-        }
-      }
-    }
-  }
+  // if (!(macAddress && macAddress !== '00:00:00:00:00:00')) {
+  //   macAddress = electron.ipcRenderer.invoke('get-mac-address');
+  // }
+  // if (!(macAddress && macAddress !== '00:00:00:00:00:00')) {
+  //   for (const iface in interfaces) {
+  //     for (const ifaceDetails of interfaces[iface]) {
+  //       // 检查接口类型和 MAC 地址
+  //       if (ifaceDetails.mac && ifaceDetails.mac !== '00:00:00:00:00:00') {
+  //         macAddress = ifaceDetails.mac;  // 返回第一个有效的 MAC 地址
+  //         return macAddress;
+  //       }
+  //     }
+  //   }
+  // }
   return macAddress;
 }
 
